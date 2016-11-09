aee88790706f702574e4821de2198c49b99ced18
philipe <felihenrique@gmail.com>
2016-11-09 15:10:02 -0300

    .

--- graficos/gerargraficos.py
+++ graficos/gerargraficos.py
@@ -24,25 +24,22 @@ pacientes_names = {
                    'aga002' : 'TCGA-AG-A002',
                    'aga02n' : 'TCGA-AG-A02N'
                    }
-
+# plt.plot(px, py, 'ro')
 con = sqlite3.connect("../banco.db")
 cursor = con.cursor()
-
-point_x = np.array([])
-point_y = np.array([])
-for pacient_id in pacientes:
-    cursor.execute("SELECT gene_name, mrna FROM expressaoRNA WHERE id_paciente=?", 
-                   (pacientes_names[pacient_id],))
-    for linha in cursor.fetchall():
-        if (linha[0] != None):
-            name = 'peptides_' + pacient_id
-            qry = "SELECT " + name + " from proteinGroups WHERE gene_names='" + linha[0] + "'"
-            cursor.execute(qry)
-            result = cursor.fetchone() 
-            if (result != None):
-                point_x = np.insert(point_x, 0, linha[1])
-                point_y = np.insert(point_y, 0, result[0])
-plt.plot(point_x, point_y)
-
-plt.show()
-
+def generate_peptides_points():
+    point_x = np.array([])
+    point_y = np.array([])
+    for pacient_id in pacientes:
+        cursor.execute("SELECT gene_name, mrna FROM expressaoRNA WHERE id_paciente=?", 
+                       (pacientes_names[pacient_id],))
+        for linha in cursor.fetchall():
+            if (linha[0] != None):
+                name = 'peptides_' + pacient_id
+                qry = "SELECT " + name + " from proteinGroups WHERE gene_names='" + linha[0] + "'"
+                cursor.execute(qry)
+                result = cursor.fetchone() 
+                if (result != None):
+                    point_x = np.insert(point_x, 0, linha[1])
+                    point_y = np.insert(point_y, 0, result[0])
+    return point_x, point_y
---
+++ installpython.cmd
@@ -0,0 +1,1 @@
+conda install pyqt numpy matplotlib pandas spyder nose
